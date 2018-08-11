cdc9bfeae9c7e17d210d9d4317a812c5d7cbb26c
Joana Palero <joana.palero24@gmail.com>
2018-08-11 13:53:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-  post '/food' do
+  post '/food_form' do
   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 end
